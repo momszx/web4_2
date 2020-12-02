04f8a4227c54e47d14a3c9f9d72142aa7544c576
Kiss Milán <kissmilan40@gmail.com>
2020-12-02 18:41:40 +0100

    Fronted with base components added

---
+++ Beadando/frontend/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ListElements from "./components/ListElements";
+
+function App() {
+  return (
+    <div >
+      <header>
+      </header>
+      <body>
+      <ListElements/>
+      </body>
+    </div>
+  );
+}
+
+export default App;
