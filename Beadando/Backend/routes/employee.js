dfac9e74516ced5112bc93dad27c2ac8da2ed0d7
momsz <donkomark@gmail.com>
2020-12-11 12:49:43 +0100

    Employee add átdolgozva

--- Beadando/Backend/routes/employee.js
+++ Beadando/Backend/routes/employee.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const MongoClient =require('mongodb').MongoClient;
 const url = "mongodb+srv://momsz:45TtSckzlyApU0v3@cluster0.apeyl.mongodb.net/myDatabase?retryWrites=true&w=majority";
 const mongodb = require('mongodb');
-const httpStatus = require('http-status-codes');
 
 /* GET element listing. */
 //List All
@@ -61,36 +60,39 @@ router.get('/findOne',function (req,res) {
 });
 /* Post one element to list. */
 //Add one element
-router.post('/add',function (req,res) {
-  req.on('data', function(chunk) {
-    let stringToJson=chunk.toString();
-    let json =JSON.parse(stringToJson);
-    console.log(json);
-    MongoClient.connect(url,function (err,db){
-      if(err) throw err;
-      let dbo= db.db("mydb");
-      dbo.collection("Employee").insertOne(json,function (err,res){
-        if(err) throw err;
-        console.log(res.insertedCount+" Elem beillesztve az adatbázisba")
-        db.close();
-      })
+router.post('/add',(req ,res)=>{
+  console.log(req.body)
+  MongoClient.connect(url,function (err,db) {
+    if (err) throw err;
+    let dbo=db.db("mydb");
+    dbo.collection("Employee").insertOne(req.body,function (err,res) {
+      if(err)throw err;
+      console.log(res.insertedCount+" Elem beillesztve az adatbázisba")
+      db.close();
     })
-    res.status(200).send('OK');
-  });
-});
+  })
+  res.status(200).send();
+})
+
 /* Update one element to list. */
 //Update one element
 router.put('/update',function (req,res) {
-  let id = new mongodb.ObjectID("5fc7e52258b20e2a69b041da");
-  let mire={ $set: {name: "Mickey", address: "Canyon 123" } };
-  MongoClient.connect(url,function (err,db){
-    if(err) throw err;
-    let dbo=db.db("mydb");
-    dbo.collection("Employee").updateOne(id,mire,function (err,result) {
-      if (err) throw err;
-      db.close();
+  req.on('data', function(chunk) {
+    let id = new mongodb.ObjectID("5fc7e52258b20e2a69b041da");
+    let mire={ $set: {name: "Mickey", address: "Canyon 123" } };
+    let stringToJson=chunk.toString();
+    let json =JSON.parse(stringToJson);
+    console.log(json);
+    /*MongoClient.connect(url,function (err,db){
+      if(err) throw err;
+      let dbo=db.db("mydb");
+      dbo.collection("Employee").updateOne(id,mire,function (err,result) {
+        if (err) throw err;
+        db.close();
 
-    })
+      })
+    })*/
+    res.status(200).send('OK');
   })
 })
 module.exports = router;
